03498049612f51d2b2af7364c1311045d48de7d5
Micholms <156813532+Micholms@users.noreply.github.com>
2025-04-30 14:35:34 +0200

    Add files via upload

--- functions/match_functions.py
+++ functions/match_functions.py
@@ -7,21 +7,23 @@ sys.path.append("../")
 def find_closest(numbers, target):
     return min(numbers, key=lambda x: abs(x- target))
 
-def find_match(numbers,target, ppm):
+def find_match(numbers,target, ppm,return_nearest=False):
     closest=find_closest(numbers, target)
   
     if abs(closest-target)<target*ppm*10**-6:
         return closest
+    if return_nearest:
+        return closest 
         
 def mz_to_m(adduct):
     mass_dict={"[M+H]+":-1.0078+0.0005,"[M+H+H]2+":2*(-1.0078+0.0005),"[M+Na]+":-22.990+0.0005,"[M+K]+":-39.098+0.0005,
-              "[M+H-H2O]+":2*(-1.0078-15.999)+0.0005,"[M+H+H2]3+":3*(-1.0078+0.0005)}
+              "[M+H-H2O]+":-1.0078-15.999+0.0005,"[M+H+H2]3+":3*(-1.0078+0.0005)}
     charge_dict={"[M+H]+":1,"[M+H+H]2+":2,"[M+Na]+":1,"[M+K]+":1,
               "[M+H-H2O]+":1,"[M+H+H2]3+":3}
     
     return mass_dict[adduct],charge_dict[adduct]
 
-def count_matches(data,target_list,ppm):#, m_type="m"):
+def count_matches(data,target_list,ppm,return_nearest=False):#, m_type="m"):
     data=data.copy()
     match=[]
     not_found=[]
@@ -29,7 +31,7 @@ def count_matches(data,target_list,ppm):#, m_type="m"):
          print("Converting m/z to m for matching")
          for m in (data.index):
             m_change,c_change=mz_to_m(data.loc[m,'Precursor_type'])
-            data.loc[m,'M']=(data.loc[m,'PrecursorMZ']+m_change)*c_change #Turn into M from m/z
+            data.loc[m,'M']=(data.loc[m,'PrecursorMZ']*c_change)+m_change #Turn into M from m/z
     else:
         data['M']=data["PEPMASS"]
     all_masses=data['M']
@@ -38,7 +40,7 @@ def count_matches(data,target_list,ppm):#, m_type="m"):
     target_INCHIKEY=target_list["INCHIKEY"]
     target_name=target_list["Sample Name"]
     for i in target_list.index:
-        m=find_match(all_masses,target_mass[i],ppm)
+        m=find_match(all_masses,target_mass[i],ppm,return_nearest)
         if m:
             match.append(target_mass[i])
             j=data.index[data['M']==m].tolist()
@@ -53,7 +55,7 @@ def count_matches(data,target_list,ppm):#, m_type="m"):
     print("Found", len(match), "/", len(target_mass), "matches at", ppm,"ppm")
     return match,not_found,data
 
-def match_one_well(df,well,ppm, values_df,int_s=False):
+def match_one_well(df,well,ppm, values_df,int_s=False,return_nearest=False):
 
     target_data=well
     print("----- Targeting", target_data)
@@ -63,11 +65,11 @@ def match_one_well(df,well,ppm, values_df,int_s=False):
     #Get list of found values in MetaboScape
     data_in=df[df["Well"]==target_data]
   
-    match,not_found,data_out=count_matches(data_in,target_list,ppm)
+    match,not_found,data_out=count_matches(data_in,target_list,ppm,return_nearest)
     if int_s:
         internal_s=pd.read_csv("first_plate/internal_standards.csv", sep=";")
         print("Internal Standard")
-        i_match,i_not_found=count_matches(data_in,internal_s,ppm)[0:-1]
+        i_match,i_not_found=count_matches(data_in,internal_s,ppm,return_nearest)[0:-1]
         return match,not_found,i_match,i_not_found,data_out
     else:
         return match,not_found, data_out
